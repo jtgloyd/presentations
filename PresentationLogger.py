69fa47c772e47a92592f42ff44b83101f9211cc9
James Todd Gloyd <james.todd.gloyd@gmail.com>
2023-03-03 11:47:00 -0500

    Manim Logger Override
    Added option to use manim logger instead of "presentations" logger.

--- PresentationLogger.py
+++ PresentationLogger.py
@@ -4,17 +4,24 @@ import sys
 # See:
 # https://docs.python.org/3.8/library/logging.html
 
-logger = logging.getLogger("presentations")
+use_manim_logger = True
+
+if use_manim_logger:
+    logger = logging.getLogger("manim")
+    pass
+else:
+    logger = logging.getLogger("presentations")
+    # logger.setLevel(logging.DEBUG)
+    # fmt = logging.Formatter(logging.BASIC_FORMAT)
+    fmt = logging.Formatter("%(levelname)s:%(filename)s:%(lineno)s:\t%(message)s")
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    pass
+
 logger.setLevel(logging.DEBUG)
-# fmt = logging.Formatter(logging.BASIC_FORMAT)
-fmt = logging.Formatter("%(levelname)s:%(filename)s:%(lineno)s:\t%(message)s")
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(fmt)
-logger.addHandler(handler)
 PRESENTATION_INFO = 17
 logging.addLevelName(PRESENTATION_INFO, 'PRESENTATION_INFO')
-
-# logger = logging.getLogger("manim")
 
 if __name__ == '__main__':
     """Stream test"""
